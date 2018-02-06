a7f3463fd1cc23d5331cead35f3f90aedc4baa5e
Nick Frost <nickfrostatx@gmail.com>
2018-02-06 13:21:28 -0800

    Add some more serious ping tests

--- test/test_conn.py
+++ test/test_conn.py
@@ -1,3 +1,4 @@
+import os
 import os.path
 import pytest
 import signal
@@ -38,6 +39,23 @@ def connect():
 
 def test_echo(proc):
     with connect() as sock:
-        packet = b'\x04\x02\x00\x14\0\0\0\0Hello world!'
+        packet = b'\x04\x02\x00\x14\x12\x34\x56\x78Hello world!'
         sock.sendall(packet)
-        assert sock.recvall(20) == b'\x04\3\x00\x14\0\0\0\0Hello world!'
+        assert sock.recvall(20) == b'\x04\3\x00\x14\x12\x34\x56\x78Hello world!'
+
+
+def test_big_echo(proc):
+    with connect() as sock1, connect() as sock2:
+        header1 = b'\x04\x02\xff\xff\xde\xad\xbe\xef'
+        header2 = b'\x04\x02\xff\xff\x11\xc0\xff\xee'
+        payload1 = os.urandom(65535 - len(header1))
+        payload2 = os.urandom(65535 - len(header2))
+        sock1.sendall(header1)
+        sock2.sendall(header2)
+        time.sleep(.05)
+        sock1.sendall(payload1)
+        assert sock1.recvall(8) == b'\x04\3\xff\xff\xde\xad\xbe\xef'
+        sock2.sendall(payload2)
+        assert sock1.recvall(len(payload1)) == payload1
+        assert sock2.recvall(8) == b'\x04\3\xff\xff\x11\xc0\xff\xee'
+        assert sock2.recvall(len(payload2)) == payload2
